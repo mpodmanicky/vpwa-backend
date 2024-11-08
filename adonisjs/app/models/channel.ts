21869c79db707090f5f806e535dfe5093e2ded21
Martin Podmanicky <mpodmanicky34@gmail.com>
2024-11-08 22:03:31 +0100

    database-work

--- adonisjs/app/models/channel.ts
+++ adonisjs/app/models/channel.ts
@@ -1,9 +1,17 @@
 import { DateTime } from 'luxon'
 import { BaseModel, column } from '@adonisjs/lucid/orm'
+import { EnumType } from 'typescript'
 
 export default class Channel extends BaseModel {
   @column({ isPrimary: true })
   declare id: number
+
+  @column()
+  declare name: string
+
+  @column()
+  declare visibility_status:
+
 
   @column.dateTime({ autoCreate: true })
   declare createdAt: DateTime
