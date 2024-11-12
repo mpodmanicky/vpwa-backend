15fad99d18e7905291d81ac9ddf4b5f4e86a292b
Martin Podmanicky <mpodmanicky34@gmail.com>
2024-11-12 13:54:06 +0100

    added routes for loginUser and registerUser

--- adonisjs/start/routes.ts
+++ adonisjs/start/routes.ts
@@ -10,3 +10,17 @@
 import router from '@adonisjs/core/services/router'
 
 router.get('/', async () => 'It works!')
+
+router.post('/registerUser', async ({ request, response }) => {
+  const credentials = request.only(['name', 'username', 'email', 'password', 'repassword'])
+
+  console.log(request.body())
+  response.status(200).send({ email: credentials.email, password: credentials.password })
+})
+
+router.post('/loginUser', async ({ request, response }) => {
+  const credentials = request.only(['email', 'password'])
+
+  console.log(request.body())
+  response.status(200).send({ email: credentials.email, password: credentials.password })
+})
