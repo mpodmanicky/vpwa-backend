d58eb58717bc4999c1051d91b0a9dd843c526b54
Martin Podmanicky <mpodmanicky34@gmail.com>
2024-11-12 22:03:41 +0100

    updated-migrations-missing-banned-users

--- adonisjs/database/migrations/1731442815281_create_user_notifications_table.ts
+++ adonisjs/database/migrations/1731442815281_create_user_notifications_table.ts
@@ -6,7 +6,13 @@ export default class extends BaseSchema {
   async up() {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id')
-
+      table.enu('notification_status', ['all', 'none', 'mentions'], {
+        useNative: true,
+        enumName: 'user_notification_status',
+        existingType: false,
+      })
+      table.integer('user_id').unsigned().references('users.id').onDelete('CASCADE')
+      table.integer('channel_id').unsigned().references('channels.id').onDelete('CASCADE')
       table.timestamp('created_at')
       table.timestamp('updated_at')
     })
